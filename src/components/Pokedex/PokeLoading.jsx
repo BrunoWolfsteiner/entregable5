4ac92afab552a26f25bc464865f920d01cf82de9
BrunoWolfsteiner <brunowolfsteiner0@gmail.com>
2023-01-03 23:15:39 -0300

    first commit

---
+++ src/components/Pokedex/PokeLoading.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './styles/PokeLoading.css'
+
+const PokeLoading = () => {
+  return (
+    <div className='loading__container'>
+        <img className='loading__img' src="./pokedex.png" alt="" />
+    </div>
+  )
+}
+
+export default PokeLoading
